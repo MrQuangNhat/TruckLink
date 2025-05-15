91f3edd5e730bb54114477dc6afd3656b8c749c3
MrQuangNhat <131015218+MrQuangNhat@users.noreply.github.com>
2025-05-15 11:10:34 +0700

    init code

--- src/server.js
+++ src/server.js
@@ -1,14 +1,33 @@
-import express from 'express';
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
 
-const app = express();
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
-const hostname = 'localhost';
-const port = 8888;
+const app = express()
 
-app.get('/', function (req, res) {
-    res.send('<h1>Hello World</h1>');
+const hostname = 'localhost'
+const port = 8888
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+    { id: 'id-2', name: 'Two' },
+    { id: 'id-3', name: 'Three' },
+    { id: 'id-4', name: 'Four' },
+    { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-    console.log(`Server TruckLink running at http://${hostname}:${port}/`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`TruckLink is running at http://${hostname}:${port}/`)
+})
